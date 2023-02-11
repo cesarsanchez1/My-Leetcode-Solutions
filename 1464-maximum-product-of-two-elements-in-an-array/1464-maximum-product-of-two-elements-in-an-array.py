876b58587d0f29ed7c01df6761253c5d94e1408b
cesarsanchez1 <60753346+cesarsanchez1@users.noreply.github.com>
2023-02-11 16:41:01 -0500

    Time: 52 ms (74.23%), Space: 13.8 MB (77.76%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -1,26 +1,7 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        freq = {}
-        highest = max(nums)
-        unique = set(nums)
-
-        for item in nums:
-            if item in freq:
-                freq[item]+=1
-            else:
-                freq[item]=1
-        
-        if freq[highest]>1:
-            return (highest-1)*(highest-1)
-        
-        secondHighest = highest
-        smallest = min(nums)
-
-        while secondHighest >= smallest:
-            secondHighest-=1
-            if secondHighest in unique:
-                return (secondHighest-1)*(highest-1)
-        return 0
+        nums.sort()
+        return (nums[len(nums)-1]-1)*(nums[len(nums)-2]-1)
 
 
 
