d0f731ceac082615386a2bdd063981037f0ecf21
cesarsanchez1 <60753346+cesarsanchez1@users.noreply.github.com>
2023-06-10 03:04:31 -0400

    Time: 249 ms (54.35%), Space: 16.6 MB (63.83%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,38 +1,19 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        n = len(s)
-        counts = [0] * 3  # Counters for a, b, c
         window_start = 0
-        substr_count = 0
-
+        result = 0
+        n = len(s)
+        count = [0]*3
+        
         for window_end in range(n):
-            # Expand the window
-            counts[ord(s[window_end]) - ord('a')] += 1
-
-            # Shrink the window until it no longer contains all characters
-            while all(counts):
-                # Count the valid substrings
-                substr_count += n-window_end
-                # Shrink the window
-                counts[ord(s[window_start]) - ord('a')] -= 1
-                window_start += 1
-
-        return substr_count
+            count[ord(s[window_end])-ord('a')]+=1
+            while all(count):
+                count[ord(s[window_start])-ord('a')]-=1
+                result += (n-window_end)
+                window_start+=1
+            
+        return result
                 
                 
-
-
-
-                
-                
-
-
-
-
-
-
-
-
-
-
+            
         
