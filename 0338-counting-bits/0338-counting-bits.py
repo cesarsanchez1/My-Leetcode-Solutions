1effdcf7f0d0061c43c9117467c8faad89f6b0b4
cesarsanchez1 <60753346+cesarsanchez1@users.noreply.github.com>
2023-07-07 00:03:47 -0400

    Time: 191 ms (18.12%), Space: 23.2 MB (33.60%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,17 +1,19 @@
-class Solution:    
+class Solution:
     def countBits(self, n: int) -> List[int]:
-        def countZero(myStr):
+        
+        def countOnes(check: str):
             counter = 0
-            for i in range(len(myStr)):
-                if myStr[i] == '1':
-                    counter+=1
+            for i in range(len(check)):
+                if check[i]=='1':
+                    counter += 1
             return counter
         
-        arr = []
+        res = []
         
         for i in range(n+1):
-            temp = bin(i)
-            tempNum = countZero(temp[2:len(temp)])
-            arr.append(tempNum)
-        return arr
+            binNum = str(bin(i))
+            res.append(countOnes(binNum[2:]))
+            
+        return res
+            
         
