88c999af0becc1a477af21fb04375a42a8f8f811
cesarsanchez1 <60753346+cesarsanchez1@users.noreply.github.com>
2023-07-07 00:08:07 -0400

    Time: 139 ms (82.54%), Space: 19.2 MB (37.60%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # To find the single element in an array where every element 
+        # appears twice except for one, you can use the XOR (exclusive OR) 
+        # operation. XORing a number with itself results in zero, so when you XOR 
+        # all the numbers in the array together, the duplicate elements cancel each 
+        # other out, leaving only the single element.
+        
         res = 0
+        
         for num in nums:
             res ^= num
         return res
