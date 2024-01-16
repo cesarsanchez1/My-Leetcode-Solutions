872f38803c898fa975471773342d84b55daeca6e
cesarsanchez1 <60753346+cesarsanchez1@users.noreply.github.com>
2024-01-15 23:12:54 -0500

    Time: 69 ms (33.40%), Space: 17.4 MB (28.94%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,31 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        chars.append("ASIJMAISCKJNJ")
+        stack = []
+        res = ""
+
+        j = 0
+        print(chars)
+
+        for letter in chars:
+            if stack and letter != stack[-1]:
+                res += stack[-1]
+                chars[j] = stack[-1]
+                j+=1
+                if 10 > len(stack) > 1:
+                    chars[j] = str(len(stack))
+                    j+=1
+                if len(stack) >= 10:
+                    size = len(str(len(stack)))
+                    temp = str(len(stack))
+                    for i in range(size):
+                        chars[j] = temp[i]
+                        j+=1
+                    
+                stack = []
+            stack.append(letter)
+        del chars[-1]
+        
+        return j
+
+
