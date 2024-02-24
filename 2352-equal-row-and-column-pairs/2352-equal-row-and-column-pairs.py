4ef948a896788aa8aeec1484b07257396c838693
Cesar Sanchez <60753346+cesarsanchez1@users.noreply.github.com>
2024-02-24 17:53:29 -0500

    Time: 380 ms (84.38%), Space: 21.7 MB (47.78%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,32 +1,34 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
         
-        arrT = []
+        rowSet = set()
+        columnSet = set()
+        columns = {}
+        rows = {}
 
-        for j in range(m):
+        for i in range(len(grid)):
+            rows[tuple(grid[i])] = rows.get(tuple(grid[i]), 0) + 1
+            rowSet.add(tuple(grid[i]))
+
+        for j in range(len(grid[0])):
             temp = []
-            for i in range(n):
+            for i in range(len(grid)):
                 temp.append(grid[i][j])
-            arrT.append(temp)
+            columns[tuple(temp)] = columns.get(tuple(temp), 0) + 1
+            columnSet.add(tuple(temp))
         
-        freq = {}
         res = 0
-        gridSet = set()
 
-        for item in grid:
-            gridSet.add(tuple(item))
-            freq[tuple(item)] = freq.get(tuple(item), 0) + 1
-
-        for item in arrT:
-            if tuple(item) in gridSet:
-                res += freq[tuple(item)]
+        for item in rowSet:
+            if item in columnSet:
+                res += rows[item]*columns[item]
         
         return res
+            
 
 
 
 
-        
-        
+            
+
+
