c6147c880f5164d269aae2e36a40a2395c5e2805
Cesar Sanchez <60753346+cesarsanchez1@users.noreply.github.com>
2024-11-09 15:40:01 -0500

    Time: 11 ms (75.62%), Space: 16.6 MB (91.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,38 +1,13 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        set1 = set(s)
-        set2 = set(t)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         
-        if set1 != set2:
-            return False
-        if len(s) != len(t):
-            return False
+        arr1 = [0]*26
+        arr2 = [0]*26
         
-        freq1 = {}
-
         for item in s:
-            if item in freq1:
-                freq1[item] += 1
-            else:
-                freq1[item] = 1
-
-        freq2 = {}
-
-        for item in t:
-            if item in freq2:
-                freq2[item] += 1
-            else:
-                freq2[item] = 1
+            arr1[ord(item)-ord('a')]+=1
         
-        for item in set1:
-            if freq1[item] != freq2[item]:
-                return False
-        return True
-
-
-
+        for item in t:
+            arr2[ord(item)-ord('a')]+=1
+        
+        return arr1 == arr2
